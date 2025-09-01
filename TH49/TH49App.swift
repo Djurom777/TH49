30a8d41e79ee6bd5d34a98de7a9353a005672bb1
DJUROM777 <djurom777@ya.ru>
2025-09-01 22:19:58 +0300

    Initial Commit

---
+++ TH49/TH49App.swift
@@ -0,0 +1,17 @@
+//
+//  TH49App.swift
+//  TH49
+//
+//  Created by IGOR on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TH49App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TH49Tests/TH49Tests.swift
@@ -0,0 +1,16 @@
+//
+//  TH49Tests.swift
+//  TH49Tests
+//
+//  Created by IGOR on 01/09/2025.
+//
+
+import Testing
+
+struct TH49Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
